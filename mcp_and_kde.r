a156a3a28a22f7f59115fa630f2059fe2a53f22d
dnbucklin <david.bucklin@gmail.com>
2016-02-02 10:29:31 -0500

    Change KDE output to Polygons, fixed KDE hole bug

--- mcp_and_kde.r
+++ mcp_and_kde.r
@@ -18,10 +18,10 @@ library(rgeos)
 
 ######CHANGE SETTINGS SECTION########
 #working directory - folder containing input data and where to export results. Set below
-setwd("/directory")           #######CHANGE THIS#########
+setwd("C:/Users/dbucklin/Desktop/test")           #######CHANGE THIS#########
 
 #load data table - this file should be in your working directory
-data1<-read.table("file.txt",header=T)                          #######CHANGE THIS#########
+data1<-read.table("all_3_red.txt",header=T)                          #######CHANGE THIS#########
 
 #standard deviation ratio settings for rescaling KDEs
 sd.ratio.min<-0.5     #######CHANGE THIS - OPTIONAL#########
@@ -97,8 +97,9 @@ for (idnum in list.ani){
         kb<-rasterFromXYZ(ka,digits=8)
         kc<-rasterToContour(kb,maxpixels=3000000,levels=c(50,95))
         kc$id<-uniqid
-        writeLinesShape(kc, paste0("utilization_distributions/",uniqid,"_kde"))
-        #
+      
+        #writeLinesShape(kc, paste0("utilization_distributions/",uniqid,"_kde"))
+        #for KDE line output, uncomment above
         capture.output(h.val,file="utilization_distributions/ud_output.txt",append=T)}
         
         else {locs.kde<-SpatialPoints(data.frame(x=locs$x/sd.x,y=locs$y/sd.y)) 
@@ -118,11 +119,74 @@ for (idnum in list.ani){
         kb<-rasterFromXYZ(ka,digits=8)
         kc<-rasterToContour(kb,maxpixels=3000000,levels=c(50,95))
         kc$id<-uniqid
-        writeLinesShape(kc, paste0("utilization_distributions/",uniqid,"_kde"))
         
+        #writeLinesShape(kc, paste0("utilization_distributions/",uniqid,"_kde"))
+        #for KDEline output, uncomment above
         print(u)
         capture.output(h.val,file="utilization_distributions/ud_output.txt",append=T)
         } 
+        
+        #begin hole removal
+        ct<-0
+        vertsort<-c(95,50)
+        #loop through each KDE level
+        for (lev in vertsort) {
+          ct<-ct+1
+          t<-subset(kc,kc@data$level==lev)
+          
+          #convert to poly
+          t1<-SpatialPolygonsDataFrame(gPolygonize(t),data=data.frame(level=seq(1,length(gPolygonize(t)),1)))
+          t2<-unionSpatialPolygons(t1,IDs=rep(as.character(ct),length(t1)))
+          
+          holetab<-data.frame(id=NA,index=NA)
+          
+          for (i in t1@data$level)
+          {
+            l<-length(t1@polygons[[i]]@Polygons)
+            if (l == 1) {next} else {
+              for (p in 1:length(t1@polygons[[i]]@Polygons)) {
+                polys<-t1@polygons[[i]]@Polygons
+                poly<-t1@polygons[[i]]@Polygons[[p]]
+                if (isTRUE(poly@hole)) {
+                  holetab<-rbind(holetab,c(i,p))
+                }
+              }
+            }
+          }
+          
+          holetab<-holetab[complete.cases(holetab),]
+          if (length(holetab[,1]) == 0) {
+            out<-SpatialPolygonsDataFrame(t2,data=data.frame(row.names=ct,level=lev))
+            assign(paste0('kernel',lev),out)
+            next
+          }
+          
+          for (m in 1:length(holetab[,1]))
+          {
+            ind<-holetab[m,]
+            hole<-t1@polygons[[ind$id]]@Polygons[[ind$index]]
+            hole@hole<-FALSE
+            hole<-Polygons(list(hole),'1')
+            hole2<-SpatialPolygons(list(hole))
+            t2<-gDifference(t2,hole2,id=as.character(ct))
+          }
+          
+          out<-SpatialPolygonsDataFrame(t2,data=data.frame(row.names=ct,level=lev))
+          assign(paste0('kernel',lev),out)
+        }
+        
+        #browser()
+        if (length(vertsort) > 1) {
+          str<-paste('kde.out<-rbind(',paste(paste('kernel',vertsort,sep=""),collapse=','),')',sep="") 
+        } else {
+          str<-paste('kde.out<-',paste('kernel',vertsort,sep=""),sep="")
+        }
+        
+        eval(parse(text=str))
+        #end hole removal
+        
+        writePolyShape(kde.out, paste0("utilization_distributions/",uniqid,"_kdepoly"))
+        
       } else {
         capture.output(c(uniqid,length(alllocs$x),length(locs$x)),file="utilization_distributions/ud_output.txt",append=T)
         print("Less than 20 mean daily locations, no kernel UD calcualated.")}
